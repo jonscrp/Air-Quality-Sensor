586df10777a76c42e8573f8432d6577fd0974508
Ric <rtoledocrow@gmail.com>
2021-03-25 10:21:30 -0400

    Update arduino_secrets.h
    added Hannah's dorm wifi

--- FeatherM0_RTC_SD_SCD30-CO2_OLED_BME280/arduino_secrets.h
+++ FeatherM0_RTC_SD_SCD30-CO2_OLED_BME280/arduino_secrets.h
@@ -1,6 +1,10 @@
-// NGENS lab wifi
+// NGENS lab pc hotspot wifi
 #define SECRET_SSID "NGENSPC1"
 #define SECRET_PASS "N63n5adm1n"
+
+// NGENS lab raspberrypi ap wifi
+//#define SECRET_SSID "NGENSRPI3"
+//#define SECRET_PASS "N63n5adm1n"
 
 // Ricardo's home wifi
 //#define SECRET_SSID "Gardsnas"
@@ -9,3 +13,7 @@
 // Amalia's home wifi
 //#define SECRET_SSID "SpectrumSetup-D0"
 //#define SECRET_PASS "sleepylawn337"
+
+// Hannah's home wifi
+//#define SECRET_SSID "SingleDigitsGuest"
+//#define SECRET_PASS ""
