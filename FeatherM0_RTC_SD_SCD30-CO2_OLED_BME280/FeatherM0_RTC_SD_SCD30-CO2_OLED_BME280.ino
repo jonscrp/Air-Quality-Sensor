49ae862e159face19e5a1f3bd01f1e75d127764e
rictolcrow <rtoledocrow@gmail.com>
2021-04-29 12:49:42 -0400

    merged Amalia's and Ricardo's latest code

--- FeatherM0_RTC_SD_SCD30-CO2_OLED_BME280/FeatherM0_RTC_SD_SCD30-CO2_OLED_BME280.ino
+++ FeatherM0_RTC_SD_SCD30-CO2_OLED_BME280/FeatherM0_RTC_SD_SCD30-CO2_OLED_BME280.ino
@@ -20,7 +20,6 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-#include <SPI.h>
 #include <WiFi101.h>
 #include "arduino_secrets.h" // wifi name and password in .h file. see tab
 
@@ -29,7 +28,6 @@
 #define BUTTON_B  6  // oled button
 #define BUTTON_C  5  // oled button
 
-
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 char ssid2[] = SECRET_SSID2;        // your network SSID (name)
@@ -37,12 +35,6 @@ char pass2[] = SECRET_PASS2;
 
 int status = WL_IDLE_STATUS;
 char server[] = "script.google.com"; // name address for Google scripts as we are communicationg with the scripg (using DNS)
-int loopCount = 0;
-float C; // CO2
-float T; // temperature
-float P; // pressure
-float H; // humidity
-bool wifion = false;
 
 WiFiSSLClient client; // make SSL client
 // these are the commands to be sent to the google script: namely add a row to last in Sheet1 with the values TBD
@@ -97,15 +89,15 @@ int lastTimeToggle = 0;
 int timeDebounce = 100;
 
 void loop(void)  {
-  loopCount++;
+
   uint8_t ctr = 0;
   stat = stat & 0xEF; // clear bit 4 for CO2 sensor
 
-  T = bme.readTemperature();
-  P = bme.readPressure() / 100; // for hPa
-  H = bme.readHumidity();
-  airSensor.setAmbientPressure(P); // update CO2 sensor to current pressure
-  
+  float Tbme = bme.readTemperature();
+  float Pbme = bme.readPressure() / 100; // for hPa
+  float RHbme = bme.readHumidity();
+
+  airSensor.setAmbientPressure(Pbme); // update CO2 sensor to current pressure
   // wait for data avail on CO2 sensor
   while (!airSensor.dataAvailable()) {
     delay(1000);
@@ -118,12 +110,9 @@ void loop(void)  {
   }
 
   // read from co2 sensor
-  digitalWrite(LED_BUILTIN, HIGH);
-  uint16_t co2 = airSensor.getCO2();
-  C = co2; // this step converts ot float
-  float temp = airSensor.getTemperature();
-  float rh = airSensor.getHumidity();
-  digitalWrite(LED_BUILTIN, LOW);
+  uint16_t CO2 = airSensor.getCO2();
+  float Tco2 = airSensor.getTemperature();
+  float RHco2 = airSensor.getHumidity();
 
   DateTime now;
   now = rtc.now(); // fetch the date + time
@@ -132,36 +121,42 @@ void loop(void)  {
   float measuredvbat = analogRead(VBATPIN) * 0.006445;
   pinMode(BUTTON_A, INPUT_PULLUP);
 
-  sprintf(outstr, "%02u:%02u:%02u,%02u:%02u:%02u,%.2d,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%x",
+  sprintf(outstr, "%02u/%02u/%02u %02u:%02u:%02u, %.2d, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %x",
           now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second(),
-          co2, temp, rh, T, P, H, measuredvbat, stat);
-  Serial.println("Date    \tTime      \tCO2\tTemp(C)\tRH(%)\tvbat(mV)\tstatus");
+          CO2, Tco2, RHco2, Tbme, Pbme, RHbme, measuredvbat, stat);
+  Serial.println("Date Time \tCO2 \tTco2 \tRHco2 \tTbme \tPbme \tRHbme \vbat(mV) \tstatus");
   Serial.println(outstr);
   logfile.println(outstr);
   logfile.flush();   // Write to disk. Uses 2048 bytes of I/O to SD card, power and takes time
 
   payloadUpload(outstr);
   if (WiFi.status() != WL_CONNECTED) {
-        Serial.println("No WiFi");
-      }
+    Serial.println("No WiFi");
+  }
 
   for (int i = 1; i <= 2; i++)  {  // 32s =2x16s sleep
     displayState = toggleButton(BUTTON_A, displayState, buttonAstate, lastTimeToggle, timeDebounce);
     if (displayState)  { // turn display on with data
       display.clearDisplay();
       display.setCursor(0, 0);
-      display.print("CO2 ppm "); display.print(co2);
+      display.print("CO2 ppm "); display.print(CO2);
       display.print("  V "); display.println(measuredvbat);
-      display.print("T C     "); display.println(T);
-      display.print("P mBar  "); display.println(P);
-      display.print("RH%     "); display.println(H); 
+      display.print("T C     "); display.println(Tbme);
+      display.print("P mBar  "); display.println(Pbme);
+      display.print("RH%     "); display.print(RHbme);
+      if (WiFi.status() == WL_CONNECTED) {
+        display.println("    w");
+      }
+      else  {
+        display.println(" no w");
+      }
       display.display();
     }
     else  {  // turn display off
       display.clearDisplay();
       display.display();
     }
-    //int sleepMS = Watchdog.sleep();// remove comment after final push
+    //    int sleepMS = Watchdog.sleep();// remove comment after final push
     delay(16000);
   }
 }
