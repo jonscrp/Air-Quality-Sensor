b64046cd3e54b689e334f7d12d4b6b53cf49b022
Ric <rtoledocrow@gmail.com>
2021-05-06 12:58:22 -0400

    Update arduino_secrets.h

--- CSL-AQI-V2.1/arduino_secrets.h
+++ CSL-AQI-V2.1/arduino_secrets.h
@@ -3,4 +3,4 @@
 #define SECRET_PASS "WiFiPassd" // if WiFi open i.e. no password see line
 
 // Google Sheets Script Deployment IDs
-//#define GSSD_ID "AKfycbz9ayTQhGrJjgftPum5e2-s6uDvvFLXmuj1AQkhrODPRNgP_QMG3bkNKPlMhDI47E7Brg" // e.g AKfycbzLiMdE-5IlsuSrTyAcxUdH3eCu7B1Sl_mVJF2-Tge5Y8Zv66d9 from the Google Sheets Script Deployment ID
+#define GSSD_ID "EnterDeploymentID" // e.g AKfycbzLiMdE-5IlsuSrTyAcxUdH3eCu7B1Sl_mVJF2-Tge5Y8Zv66d9 from the Google Sheets Script Deployment ID
