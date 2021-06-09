0494ee57eca3d8c09291332106df27d7a3ba87ba
amaliatorres2 <amaliat209@gmail.com>
2021-06-09 12:28:02 -0400

    initialize for new screen

--- CSL-AQS-V2.1/initializeOLED.ino
+++ CSL-AQS-V2.1/initializeOLED.ino
@@ -1,22 +1,17 @@
 // Adafruit OLED display 
-
-void initializeOLED()  {
-  Serial.print("OLED starting  ");
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
-    Serial.println(F("SSD1306 allocation failed"));
-    // while(1);
-  }
-  else  {
+void initializeOLEDsample()  {  
+    Serial.begin(11500);
+    
     Serial.println("OLED ok");
-    //display.display();
+
+    Serial.println("128x64 OLED FeatherWing test");
+    display.begin(0x3C, true); // Address 0x3C default
+    display.setRotation(1);
     pinMode(BUTTON_A, INPUT_PULLUP);
     pinMode(BUTTON_B, INPUT_PULLUP);
-    pinMode(BUTTON_C, INPUT_PULLUP);
+    pinMode(BUTTON_C, INPUT_PULLUP);git
     display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    //display.clearDisplay();
+    display.setTextColor(SH110X_WHITE);
     display.setCursor(0, 0);
     display.display();
-  }
 }
