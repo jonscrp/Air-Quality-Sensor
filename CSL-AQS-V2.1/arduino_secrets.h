b63698a0b1ce595418d58369d1d573cee3718a3e
rictolcrow <rtoledocrow@gmail.com>
2021-07-02 10:47:09 -0400

    general small mods:

--- CSL-AQS-V2.1/arduino_secrets.h
+++ CSL-AQS-V2.1/arduino_secrets.h
@@ -1,6 +1,6 @@
-#define SECRET_SSID "fill"
-#define SECRET_PASS "fill"
+#define SECRET_SSID "NGENSPC1"
+#define SECRET_PASS "N63n5adm1n"
 
 // Google Sheets Script Deployment IDs
 // e.g AKfycbzLiMdE-5IlsuSrTyAcxUdH3eCu7B1Sl_mVJF2-Tge5Y8Zv66d9 from the Google Sheets Script Deployment ID
-#define GSSD_ID "fill with Deployment_ID" 
+#define GSSD_ID "AKfycbwg1dJ6OSZn2LGs5DFlN1O2WLTTcgk3I6B0skmYllahBuK1RFE" 
--- CSL-AQS-V2.1/payloadUpload.ino
+++ CSL-AQS-V2.1/payloadUpload.ino
@@ -3,11 +3,10 @@
 */
 void payloadUpload(String payload) {
   int status = 0;
-  
+
   for (int i = 1; i < 5; i++) {
     status = WiFi.begin(ssid, pass);
-    delay(500);
-
+    delay(1000);
     if (WiFi.status() == WL_CONNECTED) {
       if (!client.connected()) {
         initializeClient();
@@ -26,12 +25,12 @@ void payloadUpload(String payload) {
       client.println();
       delay(200);
 
-      Serial.println("Response: ");
+      if (client.available())
+        Serial.println("Response: ");
       while (client.available()) {
         char c = client.read();
         Serial.write(c);
       }
-      Serial.println();
 
       client.stop();
       if (!client.connected()) {
@@ -44,22 +43,23 @@ void payloadUpload(String payload) {
       Serial.print("Trying to connect to Wifi : "); Serial.println(i);
     }
   }
-  if (status != WL_CONNECTED)
-    Serial.println("Continuing without WiFi"); Serial.println(status);
+  if (status != WL_CONNECTED) {
+    Serial.println("Continuing without WiFi. Status ");
+    Serial.println(status);
+  }
 }
 
 void initializeClient() {
   Serial.print("\nStarting connection to server... ");
   if (client.connectSSL(server, 443)) {      // if you get a connection, report back via serial:
-    Serial.print("connected to ");
-    Serial.println(server);
+    Serial.print("Connected to "); Serial.println(server);
     while (client.available()) {
       char c = client.read();
       Serial.write(c);
     }
   }
   else {
-    Serial.print("No connection to server: "); Serial.println(server);
-    //while (1);
+    Serial.print("Not connected to ");
+    Serial.println(server);
   }
 }
