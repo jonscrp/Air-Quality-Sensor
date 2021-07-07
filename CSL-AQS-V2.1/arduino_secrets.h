9e4baaf032e8dbe7d309894592ea528c20b9b6df
Ric <rtoledocrow@gmail.com>
2021-07-07 13:34:04 -0400

    Update arduino_secrets.h

--- CSL-AQS-V2.1/arduino_secrets.h
+++ CSL-AQS-V2.1/arduino_secrets.h
@@ -1,6 +1,6 @@
-#define SECRET_SSID "NGENSPC1"
-#define SECRET_PASS "N63n5adm1n"
+#define SECRET_SSID "fill"
+#define SECRET_PASS "fill"
 
 // Google Sheets Script Deployment IDs
 // e.g AKfycbzLiMdE-5IlsuSrTyAcxUdH3eCu7B1Sl_mVJF2-Tge5Y8Zv66d9 from the Google Sheets Script Deployment ID
-#define GSSD_ID "AKfycbwg1dJ6OSZn2LGs5DFlN1O2WLTTcgk3I6B0skmYllahBuK1RFE" 
+#define GSSD_ID "fill Deployment_ID" 
