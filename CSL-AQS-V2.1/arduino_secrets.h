48f58b65e49345acc2a4f17e00db4745a9de154a
Ric <rtoledocrow@gmail.com>
2021-07-29 14:20:15 -0400

    Update arduino_secrets.h

--- CSL-AQS-V2.1/arduino_secrets.h
+++ CSL-AQS-V2.1/arduino_secrets.h
@@ -3,5 +3,4 @@
 
 // Google Sheets Script Deployment IDs GSSD_ID
 // e.g AKfycbzLiMdE-5IlsuSrTyAcxUdH3eCu7B1Sl_mVJF2-Tge5Y8Zv66d9 from the Google Sheets Script Deployment ID
-//#define GSSD_ID "fill" // redhook2
 #define GSSD_ID "fill with GSSD_ID"
