d346a746b4aa101a5dfcef4322b7c1f10202f667
rictolcrow <rtoledocrow@gc.cuny.edu>
2021-08-10 21:12:57 -0400

    testin :# with '#' will be ignored, and an empty message aborts the commit.

--- CSL-AQS-V3/arduino_secrets.h
+++ CSL-AQS-V3/arduino_secrets.h
@@ -1,6 +1,17 @@
-#define SECRET_SSID "fill"
-#define SECRET_PASS "fill"
+//#define SECRET_SSID "Spectrumsetup-08" // Akash's setup
+//#define SECRET_PASS "scarycar678"
+
+//#define SECRET_SSID "metvendor"
+//#define SECRET_PASS "m3tv3nd0r" // check these
+
+//#define SECRET_SSID "NGENSPC1"
+//#define SECRET_PASS "N63n5adm1n" 
+
+#define SECRET_SSID "MySpectrumWiFi45-2G"
+#define SECRET_PASS "lightstatue390" 
 
 // Google Sheets Script Deployment IDs GSSD_ID
 // e.g AKfycbzLiMdE-5IlsuSrTyAcxUdH3eCu7B1Sl_mVJF2-Tge5Y8Zv66d9 from the Google Sheets Script Deployment ID
-#define GSSD_ID "fill with GSSD_ID" // CLS Met data
+//#define GSSD_ID "AKfycbygedemP6j1k4rVJEa8M7rnsCnEmFz1A4-yXB96Vg3pFEq98Ik" // CLS Met data
+//#define GSSD_ID "AKfycbwuoV76OpcOnZ2cDhJRnhBY_HiyfAIpdr60KxffdZLH7ycHJ24" // Akash Sensor AQS data spreadsheet 
+#define GSSD_ID "AKfycbzIP88-5x87WyVCQXpeFnmUE5_KmHEY45LINRmlPhVlEUJu7aU" // Julia's Sensor Data Spreadsheet
