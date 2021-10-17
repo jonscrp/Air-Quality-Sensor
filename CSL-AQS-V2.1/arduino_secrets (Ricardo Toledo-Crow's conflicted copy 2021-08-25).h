b970fb98cdcb581f410ebf62990ad9765a7475af
amaliatorres2 <52719960+amaliatorres2@users.noreply.github.com>
2021-10-17 12:57:19 -0400

    Delete CSL-AQS-V2.1 directory

--- CSL-AQS-V2.1/arduino_secrets (Ricardo Toledo-Crow's conflicted copy 2021-08-25).h
+++
@@ -1,8 +0,0 @@
-//#define SECRET_SSID "Red Hook WIFI"
-//#define SECRET_PASS ""
-#define SECRET_SSID "Portalis"
-#define SECRET_PASS "bomdiggity"
-
-// Google Sheets Script Deployment IDs
-// e.g AKfycbzLiMdE-5IlsuSrTyAcxUdH3eCu7B1Sl_mVJF2-Tge5Y8Zv66d9 from the Google Sheets Script Deployment ID
-#define GSSD_ID "AKfycbwAx8yC3wKCNubX2CgNsdwLIZCEm6WH7fae97oKELqR1_G2GLhWWlDEkdMdAVtHy6k6sg"
